5d6ab2f9d628efa4bfec1648ef95a08477a89240
ricardo-s-c <ricardo.dev.com@gmail.com>
2024-05-07 20:38:43 -0300

    projeto criado

---
+++ src/main/java/com/dev/ricardo/apitestuser/ApiTestUserApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ricardo.apitestuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/ricardo/apitestuser/ApiTestUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ricardo.apitestuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
