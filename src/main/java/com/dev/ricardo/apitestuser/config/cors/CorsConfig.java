346f866635a10f2bb64f19ee83d2aeaf317a88b1
ricardo-s-c <ricardo.dev.com@gmail.com>
2024-05-07 22:19:49 -0300

    config cors

---
+++ src/main/java/com/dev/ricardo/apitestuser/config/cors/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.dev.ricardo.apitestuser.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
