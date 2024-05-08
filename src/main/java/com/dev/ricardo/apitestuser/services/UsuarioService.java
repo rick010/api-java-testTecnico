43b1036b17191a6c984cce08d5cca3e8845216fd
ricardo-s-c <ricardo.dev.com@gmail.com>
2024-05-07 22:37:46 -0300

    dependencia postgres incluida no .pom.xml

--- src/main/java/com/dev/ricardo/apitestuser/services/UsuarioService.java
+++ src/main/java/com/dev/ricardo/apitestuser/services/UsuarioService.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
